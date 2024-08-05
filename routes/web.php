206e3382ceb710dbb94eeec04edf2256da226020
SandyaNW <rafiopang@gmail.com>
2024-08-05 14:25:55 +0700

    index karo routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/category', function () {
+    return view('category');
+});
+
+Route::get('/search-result', function () {
+    return view('search-result');
+});
+
+Route::get('/single-post', function () {
+    return view('single-post');
+});
+
